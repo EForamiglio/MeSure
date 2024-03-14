76bbc6cf17dcd30dc5bd004c1bb32272ddfde7ea
EForamiglio <erik.foramiglio.vieira@gmail.com>
2024-03-14 20:15:33 -0300

    1.1 adicionando acionamento de alarme

--- main/main.ino
+++ main/main.ino
@@ -4,6 +4,7 @@
 
 #define DHTPIN A1
 #define DHTTYPE DHT11
+#define tempoBuzzer 10
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
@@ -12,10 +13,16 @@ float umidadeOld, temperaturaOld;
 float umidade, temperatura;
 unsigned long tempo;
 int inicio = 0;
+int ledPin = 6;
+int buzzerPin = 7;
+int frequencia = 0;
+int Pinofalante = 10;
 
 void setup() {
   Serial.begin(9600);
 
+  pinMode(ledPin, OUTPUT);
+  pinMode(buzzerPin,OUTPUT);
   lcd.init();
   lcd.setBacklight(HIGH);
 
@@ -34,7 +41,11 @@ void loop() {
     tempo = millis();
     inicio++;
   }
-  
+
+  if (umidade < 30 || umidade > 50 || temperatura < 15 || temperatura > 25) {
+//    acionaAlarme(); 
+  }
+
   if(millis() - tempo == 15000) {
     tempo = millis();
 
@@ -50,13 +61,30 @@ void loop() {
   }
 }
 
+void acionaAlarme() {
+  for (frequencia = 150; frequencia < 1800; frequencia += 1)
+  {
+    tone(buzzerPin, frequencia, tempoBuzzer);
+    delay(1);
+  }
+  digitalWrite(ledPin, HIGH);
+    delay(10);
+  for (frequencia = 1800; frequencia > 150; frequencia -= 1)
+  {
+    tone(buzzerPin, frequencia, tempoBuzzer);
+    delay(1);
+  }
+  digitalWrite(ledPin, LOW);
+  delay(10);
+}
+
 void printaValores(float temp, float umd) {
   lcd.setCursor(0, 0);
-     lcd.print("U: ");
-     lcd.print(umd);
-     lcd.print(" %");
-     lcd.setCursor(0, 1);
-     lcd.print("T: ");
-     lcd.print(temp);
-     lcd.print(" C");
+  lcd.print("U: ");
+  lcd.print(umd);
+  lcd.print(" %");
+  lcd.setCursor(0, 1);
+  lcd.print("T: ");
+  lcd.print(temp);
+  lcd.print(" C");
 }
